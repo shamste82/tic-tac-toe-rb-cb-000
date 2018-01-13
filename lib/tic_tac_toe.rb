0d7f8bdea13223928d6ac0e529523b67e9dbe479
Steffen Hammelow-Berg <shammelow@gmail.com>
2018-01-13 03:25:06 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -99,3 +99,14 @@ def over?(board)
   end
   return false
 end
+
+def winner(board)
+  combination = won?(board)
+  if !combination
+    return nil
+  end
+  if board[combination[0]] == "X" && board[combination[1]] == "X" && board[combination[2]] == "X"
+    return "X"
+  elsif board[combination[0]] == "O" && board[combination[1]] == "O" && board[combination[2]] == "O"
+    return "O"
+  end
